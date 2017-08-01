6aeed2af93745affc5b51d60b500750838e5360b
Akash Varshney <AV95634@TechMahindra.com>
2017-08-01 14:36:16 +0530

    updated controller and service methods

--- public/js/controllers/todoController.js
+++ public/js/controllers/todoController.js
@@ -1,4 +1,45 @@
 var app = angular.module('myTodo',[]);
-var myTodoController = app.controller('todoController',function($scope){
-    $scope.TestValue = "Hello WOrld";
-});
+var myTodoController = app.controller('todoController',['$scope','$http','Todo',function($scope,$http,Todo){
+    $scope.formData = {};
+    $scope.loading = true;
+    
+    Todo.get()
+        .success (function(data){
+            $scope.todos = data;
+            $scope.loading = false;
+        });
+
+   // CREATE ==================================================================
+		// when submitting the add form, send the text to the node API
+		$scope.createTodo = function() {
+
+			// validate the formData to make sure that something is there
+			// if form is empty, nothing will happen
+			if ($scope.formData.text != undefined) {
+				$scope.loading = true;
+
+				// call the create function from our service (returns a promise object)
+				Todos.create($scope.formData)
+
+					// if successful creation, call our get function to get all the new todos
+					.success(function(data) {
+						$scope.loading = false;
+						$scope.formData = {}; // clear the form so our user is ready to enter another
+						$scope.todos = data; // assign our new list of todos
+					});
+			}
+        };
+        
+        // DELETE ==================================================================
+		// delete a todo after checking it
+		$scope.deleteTodo = function(id) {
+			$scope.loading = true;
+
+			Todos.delete(id)
+				// if successful creation, call our get function to get all the new todos
+				.success(function(data) {
+					$scope.loading = false;
+					$scope.todos = data; // assign our new list of todos
+				});
+		};
+}]);
--- public/js/services/todoService.js
+++ public/js/services/todoService.js
@@ -1,5 +1,15 @@
 //create a service for todo operations
 angular.module('todoService', [])
     .factory('Todo',['$http',function($http){
-
+        return{
+            get: function(){
+                return $http.get('api/todos');
+            },
+            create: function(){
+                return $http.post('api/todos',todoData);
+            },
+            delete:function(){
+                return $http.delete('api/todos'+ id);
+            }
+        }
     }]);
